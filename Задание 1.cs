c3a3616e5df0a4a4bf80b6f7e28f7da33a6b17a9
user <user@WIN-SS6N9OIOE40>
2022-10-27 20:30:40 +0300

    Добавьте файлы проекта.

---
+++ Задание 1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Задание1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 80; i > 9; i = i - 2)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
